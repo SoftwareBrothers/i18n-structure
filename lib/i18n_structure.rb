f7afde6ed637c04c27d616891ddd2e2c899db3e6
Wojciech Krysiak <wojciech.krysiak@kmpgroup.pl>
2013-11-30 19:00:10 +0100

    add some view helpers

--- lib/i18n_structure.rb
+++ lib/i18n_structure.rb
@@ -1,6 +1,8 @@
 require "i18n_structure/version"
 require "i18n_structure/i18n_extension"
 
+ActionView::Base.send :include, I18nStructure::LocaleHelper
+
 module I18nStructure
   
 end
---
+++ lib/i18n_structure/locale_helper.rb
@@ -0,0 +1,15 @@
+module I18nStructure
+  module LocaleHelper
+    [:labels, :tooltips, :attributes, :collections].each do |kind|
+      define_method("translate_#{kind.to_s.singularize}") do |*args|
+        I18n.send("translate_#{kind.to_s.singularize}", *args)
+      end
+      alias_method "t#{kind.to_s[0]}", "translate_#{kind.to_s.singularize}"
+
+      define_method("translate_#{kind.to_s.singularize}_exist?") do |*args|
+        I18n.send("translate_#{kind.to_s.singularize}_exist?", *args)
+      end
+      alias_method "t#{kind.to_s[0]}?", "translate_#{kind.to_s.singularize}_exist?"
+    end
+  end
+end
