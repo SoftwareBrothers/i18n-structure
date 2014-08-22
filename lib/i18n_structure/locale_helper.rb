abf8bf788f6a1ec243f90b803092eee335deaa32
Wojtek Krysiak <wojciech.krysiak@kmpgroup.pl>
2014-08-22 23:38:19 +0200

    add helpers

--- lib/i18n_structure/locale_helper.rb
+++ lib/i18n_structure/locale_helper.rb
@@ -1,6 +1,6 @@
 module I18nStructure
   module LocaleHelper
-    [:labels, :tooltips, :attributes, :collections].each do |kind|
+    [:labels, :tooltips, :attributes].each do |kind|
       define_method("translate_#{kind.to_s.singularize}") do |*args|
         I18n.send("translate_#{kind.to_s.singularize}", *args)
       end
@@ -10,6 +10,12 @@ module I18nStructure
         I18n.send("translate_#{kind.to_s.singularize}_exist?", *args)
       end
       alias_method "t#{kind.to_s[0]}?", "translate_#{kind.to_s.singularize}_exist?"
+    end
+    [:collections, :views].each do |kind|
+      define_method("translate_#{kind.to_s.singularize}") do |*args|
+        I18n.send("translate_#{kind.to_s.singularize}", *args)
+      end
+      alias_method "t#{kind.to_s[0]}", "translate_#{kind.to_s.singularize}"
     end
   end
 end
